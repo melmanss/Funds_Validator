a241bc7f5e8b9385856a56a5a96801608b44aad7
melmanss <cfhfyjdbx@gmail.com>
2025-03-25 14:10:09 +0200

    New commit

--- src/app/FundsException.java
+++ src/app/FundsException.java
@@ -1,4 +1,10 @@
+
+// Клас для кастомного винятку
 package app;
 
-public class FundsException {
+public class FundsException extends Exception { //Наслідування від Exception
+
+    public FundsException(String message) {
+        super(message);
+    }
 }
--- src/app/Main.java
+++ src/app/Main.java
@@ -1,15 +1,48 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+package app;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.util.Scanner;
+
+// Головний клас програми
+public class Main {
+
+    static double balance;
+
+    public static void main(String[] args) {
+        balance = getBalance(); // Отримання початкового балансу
+        validateAmount(balance, getAmount());
+    }
+
+    // Метод для отримання балансу
+    private static double getBalance() {
+        return 1000.00; // Наявні кошти на рахунку
+    }
+
+    // Метод для введення суми покупки
+    private static double getAmount() {
+        System.out.printf("Balance is USD %.2f.%n" +
+                "Enter purchase amount, USD: ", balance);
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextDouble();
+    }
+
+    // Метод валідації наявних коштів
+    private static void validateAmount(double balance, double withdrawal) {
+        if (withdrawal > balance) { // Перевірка на недостатню кількість коштів
+            try {
+                throw new FundsException("Insufficient funds!"); // Генерація винятку
+            } catch (FundsException ex) {
+                System.out.println(ex.getMessage()); // Виведення повідомлення про помилку
+            }
+        } else {
+            balance = getBalance(balance, withdrawal);
+            System.out.printf("Funds are OK. Purchase paid." +
+                    "%nBalance is USD %.2f", balance);
         }
+    }
+
+    /* Метод розрахунку наявних коштів на рахунку
+     після зняття певної суми коштів*/
+    private static double getBalance(double balance, double withdrawal) {
+        return balance - withdrawal; // Повернення нового балансу
     }
 }
